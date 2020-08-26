a5a897567ff6a7f49a38fa3c79fe593fc6516186
Nafiz Fahad <Fahad Nafiz>
2020-08-26 19:30:56 +0600

    hey

--- Task/src/Test.java
+++ Task/src/Test.java
@@ -1,7 +1,8 @@
 
 public class Test {
 	 public static void main(String[] args) {
-		    System.out.println("Hi i am Nafiz");
+		    System.out.println("Hi i am Nafiz Fahad and i reads in AIUB");
+		    
 		  }
 
 }
