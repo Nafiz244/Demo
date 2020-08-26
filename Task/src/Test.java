2443c92507a9442aebc5c708a1d80555186caafe
Nafiz Fahad <Fahad Nafiz>
2020-08-26 19:33:00 +0600

    Hey u

--- Task/src/Test.java
+++ Task/src/Test.java
@@ -1,7 +1,7 @@
 
 public class Test {
 	 public static void main(String[] args) {
-		    System.out.println("Hi i am Nafiz Fahad and i reads in AIUB");
+		    System.out.println("Hi i am Nafiz Fahad and i reads in AIUB in 2nd semester");
 		    
 		  }
 
